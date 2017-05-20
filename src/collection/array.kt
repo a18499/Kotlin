aa467be59fc3a45eb91b53739e1147179f5e8f02
peter <a184999@gmail.com>
2017-05-20 21:45:19 +0800

    [New]Add array example

---
+++ src/collection/array.kt
@@ -0,0 +1,17 @@
+package collection
+
+/**
+ * Created by a1849 on 2017/5/20.
+ */
+
+fun main(args: Array<String>) {
+    var arraylist = arrayListOf<String>()
+    arraylist.add("Test")
+    arraylist.add("Test1")
+    arraylist.add("Test2")
+
+
+    for (String  in arraylist){
+        println(String)
+    }
+}
